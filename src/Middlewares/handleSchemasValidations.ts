8ab69f661e523ad2ee7f3d22e4a428be3dd8b71e
CarlosWTMachado <carloswtmachado@gmail.com>
2022-09-03 16:05:40 -0300

    feat: create card schema validation

--- src/Middlewares/handleSchemasValidations.ts
+++ src/Middlewares/handleSchemasValidations.ts
@@ -1,9 +1,10 @@
 export default function schemaValidate (object: any, schema: any) {
 	const validate = schema.validate(object);
 	if(validate.error) {
+		console.log(validate.error)
 		throw {
 			type: 'BadRequest',
-			message: `Data validation: ${validate.error.details}`
+			message: `Data validation: ${validate.error.details.map((v:any) => v.message)}`
 		}
 	}
 }
